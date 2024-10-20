1d934e316f8c6e4112a503f5f533ddbf3e863cae
VanPhan7761 <vanmade93729@gmail.com>
2024-10-20 11:54:28 -0400

    Setting up relevant plugins (FMOD and Sessions)

---
+++ Source/StoneSoldiers/StoneSoldiersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StoneSoldiersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STONESOLDIERS_API AStoneSoldiersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
